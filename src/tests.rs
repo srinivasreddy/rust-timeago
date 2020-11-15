a37f1e1c64e21681a1b26a8733d7cae7caf31b6f
Srinivas Reddy Thatiparthy <thatiparthysreenivas@gmail.com>
2020-11-16 00:39:29 +0530

    Release 0.0.1-alpha

--- src/tests.rs
+++ src/tests.rs
@@ -1,11 +1,178 @@
 #[cfg(test)]
 mod tests {
-    use crate::time_ago::TimeAgo;
-    use std::time::{Duration, SystemTime};
+    use crate::time_ago::{Config, TimeAgo, TimeType};
+    use std::time::{Duration, Instant, SystemTime};
 
     #[test]
-    fn test_time_ago_convert() {
-        assert_eq!(TimeAgo::now().convert(), "just now");
-        assert_eq!(TimeAgo::from_system_time(SystemTime::UNIX_EPOCH))
+    fn test_time_ago_with_config() {
+        let config = Config {
+            is_years: false,
+            is_weeks: false,
+            is_months: false,
+        };
+
+        let custom = Config {
+            is_years: true,
+            is_weeks: true,
+            is_months: true,
+        };
+        let a = TimeAgo::with_config(config, TimeType::SystemTime(SystemTime::now()));
+        assert_eq!(a.convert(), "just now");
+        let b = TimeAgo::with_config(config, TimeType::Duration(Duration::from_secs(1)));
+        assert_eq!(b.convert(), "just now");
+        let c = TimeAgo::with_config(config, TimeType::Instant(Instant::now()));
+        assert_eq!(c.convert(), "just now");
+        assert_eq!(
+            TimeAgo::with_config(custom, TimeType::Duration(Duration::from_secs(5))).convert(),
+            "5 seconds ago"
+        );
+        assert_eq!(
+            TimeAgo::with_config(custom, TimeType::Duration(Duration::from_secs(60))).convert(),
+            "1 minute ago"
+        );
+        assert_eq!(
+            TimeAgo::with_config(custom, TimeType::Duration(Duration::from_secs(120))).convert(),
+            "2 minutes ago"
+        );
+        assert_eq!(
+            TimeAgo::with_config(custom, TimeType::Duration(Duration::from_secs(60 * 60)))
+                .convert(),
+            "1 hour ago"
+        );
+        assert_eq!(
+            TimeAgo::with_config(custom, TimeType::Duration(Duration::from_secs(60 * 60 * 2)))
+                .convert(),
+            "2 hours ago"
+        );
+        assert_eq!(
+            TimeAgo::with_config(
+                custom,
+                TimeType::Duration(Duration::from_secs(60 * 60 * 23))
+            )
+            .convert(),
+            "23 hours ago"
+        );
+        assert_eq!(
+            TimeAgo::with_config(
+                custom,
+                TimeType::Duration(Duration::from_secs(60 * 60 * 24))
+            )
+            .convert(),
+            "yesterday"
+        );
+        assert_eq!(
+            TimeAgo::with_config(
+                custom,
+                TimeType::Duration(Duration::from_secs(60 * 60 * 24 * 7))
+            )
+            .convert(),
+            "1 week ago"
+        );
+        assert_eq!(
+            TimeAgo::with_config(
+                custom,
+                TimeType::Duration(Duration::from_secs(60 * 60 * 24 * 7 * 3))
+            )
+            .convert(),
+            "3 weeks ago"
+        );
+        assert_eq!(
+            TimeAgo::with_config(
+                custom,
+                TimeType::Duration(Duration::from_secs((60 * 60 * 24 * 7 * 4) - 1))
+            )
+            .convert(),
+            "3 weeks ago"
+        );
+        assert_eq!(
+            TimeAgo::with_config(
+                custom,
+                TimeType::Duration(Duration::from_secs(60 * 60 * 24 * 30))
+            )
+            .convert(),
+            "1 month ago"
+        );
+        assert_eq!(
+            TimeAgo::with_config(
+                custom,
+                TimeType::Duration(Duration::from_secs(60 * 60 * 24 * 30 * 2))
+            )
+            .convert(),
+            "2 months ago"
+        );
+        assert_eq!(
+            TimeAgo::with_config(
+                custom,
+                TimeType::Duration(Duration::from_secs(60 * 60 * 24 * 30 * 12)) // duration calculates 365.25* 60 * 60 * 24 for an year
+            )
+            .convert(),
+            "11 months ago"
+        );
+
+        assert_eq!(
+            TimeAgo::with_config(
+                custom,
+                TimeType::Duration(Duration::from_secs(60 * 60 * 24 * 366)) // duration calculates 365.25* 60 * 60 * 24 for an year
+            )
+            .convert(),
+            "1 year ago"
+        );
+
+        assert_eq!(
+            TimeAgo::with_config(
+                custom,
+                TimeType::Duration(Duration::from_secs(
+                    (60.0 * 60.0 * 24.0 * 365.25 * 2.0) as u64 - 1
+                )) // duration calculates 365.25* 60 * 60 * 24 for an year
+            )
+            .convert(),
+            "1 year ago"
+        );
+
+        assert_eq!(
+            TimeAgo::with_config(
+                custom,
+                TimeType::Duration(Duration::from_secs(
+                    (60.0 * 60.0 * 24.0 * 365.25 * 2.0) as u64
+                )) // duration calculates 365.25* 60 * 60 * 24 for an year
+            )
+            .convert(),
+            "2 years ago"
+        );
+
+        assert_eq!(
+            TimeAgo::with_config(
+                custom,
+                TimeType::Duration(Duration::from_secs(
+                    (60.0 * 60.0 * 24.0 * 365.25 * 51.0) as u64
+                )) // duration calculates 365.25* 60 * 60 * 24 for an year
+            )
+            .convert(),
+            "51 years ago"
+        );
+        assert_eq!(
+            TimeAgo::with_config(
+                custom,
+                TimeType::Duration(Duration::from_secs(
+                    (60.0 * 60.0 * 24.0 * 365.25 * 100.0) as u64
+                )) // duration calculates 365.25* 60 * 60 * 24 for an year
+            )
+            .convert(),
+            "invalid string"
+        );
+        // assert_eq!(
+        //     TimeAgo::with_config(
+        //         Config{
+        //             is_months: false,
+        //             is_weeks: false,
+        //             is_years: false
+        //         },
+        //         TimeType::Duration(Duration::from_secs(
+        //             (60.0 * 60.0 * 24.0 * 365.25 * 50.0) as u64
+        //         )) // duration calculates 365.25* 60 * 60 * 24 for an year
+        //     )
+        //         .convert(),
+        //     "Nov 1970 at 17:07:20"
+        // );
     }
 }
--- src/time_ago.rs
+++ src/time_ago.rs
@@ -1,44 +1,172 @@
-use std::fmt;
-use std::time::{Duration, SystemTime, UNIX_EPOCH};
+use chrono::{TimeZone, Utc};
+use std::cmp::min;
+use std::time::{Duration, Instant, SystemTime};
 
 #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub enum TimeAgo {
-    SystemTime(SystemTime),
+pub struct TimeAgo {
+    config: Config,
+    time_type: TimeType,
+}
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum TimeType {
+    Instant(Instant),
     Duration(Duration),
+    SystemTime(SystemTime),
+}
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct Config {
+    pub is_weeks: bool,
+    pub is_months: bool,
+    pub is_years: bool,
+}
+
+// Default implementation of Config
+// is_weeks: false -> "23 day(s) ago" is displayed instead of "1 week(s) ago".
+// is_months: false -> "Nov 20 at 11:30"  is displayed instead of "1 month(s) ago".
+// is_years: false -> "Nov 10 at 21:23" is displayed instead of "10 years ago"
+
+impl Default for Config {
+    fn default() -> Config {
+        Config {
+            is_weeks: false,
+            is_months: false,
+            is_years: false,
+        }
+    }
 }
 
 impl TimeAgo {
-    pub fn duration(duration: Duration) -> TimeAgo {
-        TimeAgo::Duration(duration)
+    pub fn with_config(config: Config, time_type: TimeType) -> TimeAgo {
+        TimeAgo { config, time_type }
     }
-
-    pub fn now() -> TimeAgo {
-        TimeAgo::SystemTime(SystemTime::now())
+    pub fn from_duration(config: Config, duration: Duration) -> TimeAgo {
+        TimeAgo {
+            config,
+            time_type: TimeType::Duration(duration),
+        }
     }
-    pub fn from_system_time(system_time: SystemTime) -> TimeAgo {
-        TimeAgo::SystemTime(system_time)
+    pub fn from_system_time(config: Config, system_time: SystemTime) -> TimeAgo {
+        TimeAgo {
+            config,
+            time_type: TimeType::SystemTime(system_time),
+        }
+    }
+    pub fn now(config: Config) -> TimeAgo {
+        Self::from_instant(config, Instant::now())
+    }
+    pub fn from_instant(config: Config, instant: Instant) -> TimeAgo {
+        TimeAgo {
+            config,
+            time_type: TimeType::Instant(instant),
+        }
     }
 
     pub fn convert(&self) -> String {
-        let seconds = match &self {
-            TimeAgo::SystemTime(value) => {
-                value.duration_since(SystemTime::now()).unwrap().as_secs()
+        let current_sec_function = || {
+            SystemTime::now()
+                .duration_since(SystemTime::UNIX_EPOCH)
+                .unwrap()
+                .as_secs()
+        };
+        let (seconds, epoch_seconds) = match &self.time_type {
+            TimeType::SystemTime(value) => (
+                SystemTime::now()
+                    .duration_since(*value)
+                    .unwrap_or(Duration::from_secs(0))
+                    .as_secs(),
+                value
+                    .duration_since(SystemTime::UNIX_EPOCH)
+                    .unwrap()
+                    .as_secs(),
+            ),
+            TimeType::Instant(value) => {
+                let seconds = Instant::now().duration_since(*value).as_secs();
+                (seconds, current_sec_function().wrapping_sub(seconds))
             }
-            TimeAgo::Duration(value) => value.as_secs(),
+            TimeType::Duration(value) => {
+                let seconds = value.as_secs();
+                (seconds, current_sec_function().wrapping_sub(seconds))
+            }
         };
         match seconds {
+            // 0 to 1 second is just now
             (0..=1) => "just now".to_string(),
+            // 2 to 59 seconds ago
             (2..=59) => format!("{} seconds ago", seconds),
+            // 60 seconds to 119 seconds
             (60..=119) => "1 minute ago".to_string(),
-            (120..=3600) => format!("{} minutes ago", seconds),
-            _ => seconds.to_string(),
+            // 2 minutes to 59 minutes 59 seconds
+            (120..=3599) => format!("{} minutes ago", seconds / 60),
+            //1 hour to 2 hours
+            (3600..=7199) => "1 hour ago".to_string(),
+            //2 hours to 23 hours 59 minutes 59 seconds
+            (7200..=86_399) => format!("{} hours ago", seconds / (60 * 60)),
+            //1 day to 1 day 23 hours 59 minutes 59 seconds,
+            (86_400..=172_799) => "yesterday".to_string(),
+            //2 days to 6 days 23 hours 59 minutes 59 seconds
+            (172_800..=604_799) => format!("{} days ago", seconds / (60 * 60 * 24)),
+            //1 week to 1 week 6 days 23 hours 59 minutes 59 seconds
+            (604_800..=1_209_599) => {
+                if self.config.is_weeks {
+                    "1 week ago".to_string()
+                } else {
+                    Utc.timestamp(epoch_seconds as i64, 0)
+                        .format("%h %Y at %X")
+                        .to_string()
+                }
+            }
+            //2 weeks to 29 days 23 hours 59 minutes 59 seconds
+            (1_209_600..=2_591_999) => {
+                if self.config.is_weeks {
+                    format!("{} weeks ago", seconds / (60 * 60 * 24 * 7))
+                } else {
+                    Utc.timestamp(epoch_seconds as i64, 0)
+                        .format("%h %Y at %X")
+                        .to_string()
+                }
+            }
+            //1 month to 1 month 29 days 23 hours 59 minutes 59 seconds
+            (2_592_000..=5_183_999) => {
+                if self.config.is_months {
+                    "1 month ago".to_string()
+                } else {
+                    Utc.timestamp(epoch_seconds as i64, 0)
+                        .format("%h %Y at %X")
+                        .to_string()
+                }
+            }
+            //2 months to 365.25 days
+            (5_184_000..=31_557_599) => {
+                if self.config.is_months {
+                    format!("{} months ago", min(seconds / (60 * 60 * 24 * 30), 11))
+                } else {
+                    Utc.timestamp(epoch_seconds as i64, 0)
+                        .format("%h %Y at %X")
+                        .to_string()
+                }
+            }
+            // 1 year(365.25 days) to 1 year 11 months 29 days 23 hours 59 minutes 59 seconds - (2*365.25 days) - 1 second
+            (31_557_600..=63_115_199) => {
+                if self.config.is_years {
+                    "1 year ago".to_string()
+                } else {
+                    Utc.timestamp(epoch_seconds as i64, 0)
+                        .format("%h %Y at %X")
+                        .to_string()
+                }
+            }
+            // 2 years to 99 years ago.
+            (63_115_200..=3155759999) => {
+                if self.config.is_years {
+                    format!("{} years ago", seconds / (60 * 60 * 24 * 365))
+                } else {
+                    Utc.timestamp(epoch_seconds as i64, 0)
+                        .format("%h %Y at %X")
+                        .to_string()
+                }
+            }
+            //100 years and ago
+            (3155760000..=std::u64::MAX) => "invalid string".to_string(),
         }
     }
-    //
-    // pub fn convert_with_locale(&self, String: _locale) -> String {
-    //     match &self {
-    //         TimeAgo::SystemTime(_value) => "Helloworld!!!".to_string(),
-    //         TimeAgo::Duration(_value) => "Helloworld!!!".to_string(),
-    //     }
-    // }
 }
